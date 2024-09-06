5c3e1ebdd996293d2b191bd0e05383394c5848a8
kaorutaniwaki <taniwaki.kaoru@gmail.com>
2024-09-07 06:01:15 +0900

    初回コミット

---
+++ app/src/main/java/net/rakustudy/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package net.rakustudy.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
